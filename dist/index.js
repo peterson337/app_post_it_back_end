2894367c7e8bcc06d9830d709b40160c00e76c63
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2025-04-09 13:05:41 -0300

    teste

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const teste = (a) => {
+    console.log(a + 2);
+};
+teste(5);
---
+++ index.ts
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+const teste = (a: number) => {
+  console.log(a + 2);
+};
+
+teste(5);
